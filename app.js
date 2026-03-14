4fe9f792645019c7458dd1e236d11d9fbec3f3cb
Shara26 <sharatadas26@gmail.com>
2026-03-14 23:57:27 +0530

    Added Reviews Option

--- app.js
+++ app.js
@@ -7,7 +7,8 @@ const methodOverride = require("method-override");
 const ejsMate = require("ejs-mate");
 const wrapAsync = require("./utils/wrapAsync.js");
 const ExpressError = require("./utils/ExpressError.js");
-const { listingSchema} = require("./schema.js");
+const { listingSchema, reviewSchema } = require("./schema.js");
+const Review = require("./models/review.js");
 
 const MONGO_URL = "mongodb://127.0.0.1:27017/wanderlust";
 
@@ -44,6 +45,18 @@ const validateListing = (req, res, next) => {
      }
 };
 
+const validateReview = (req, res, next) => {
+    let {error} = reviewSchema.validate(req.body);
+    if(error) {
+        let errMsg = error.details.map((el) => el.message).join(",");
+        throw new ExpressError(400, errMsg);
+     }else {
+        next();
+     }
+};
+
+
+
 //Index Route
 app.get("/listings", wrapAsync(async (req, res) => {
     const allListings = await Listing.find({});
@@ -58,7 +71,7 @@ app.get("/listings/new", (req, res) => {
 //Show Route
 app.get("/listings/:id", wrapAsync(async (req, res) => {
     let {id} = req.params;
-    const listing = await Listing.findById(id);
+    const listing = await Listing.findById(id).populate("reviews");
     res.render("listings/show.ejs", {listing});
 }));
 
@@ -110,6 +123,31 @@ app.delete("/listings/:id",wrapAsync (async (req, res) => {
 })
 );
 
+//Reviews
+//Post Review Route
+app.post("/listings/:id/reviews", validateReview, wrapAsync(async(req, res) => {
+    let listing = await Listing.findById(req.params.id);
+    let newReview = new Review(req.body.review);
+
+    listing.reviews.push(newReview);
+    
+    await newReview.save();
+    await listing.save();
+
+    res.redirect(`/listings/${listing._id}`);
+}));
+
+//Delete Review Route
+app.delete(
+    "/listings/:id/reviews/:reviewId",
+     wrapAsync(async (req, res) => {
+
+        const { id, reviewId } = req.params;
+        await Listing.findByIdAndUpdate(id, {$pull: {reviews: reviewId}});
+        await Review.findByIdAndDelete(reviewId);
+
+        res.redirect(`/listings/${id}`);
+     }));
 
 
 
--- models/review.js
+++ models/review.js
@@ -4,6 +4,7 @@ const Schema = mongoose.Schema;
 const reviewSchema = new Schema({
     comment: String,
     rating: {
+        type: Number,
         min: 1,
         max: 5
     },
